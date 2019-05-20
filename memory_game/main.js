09babc966ee69b34ffab09152649f40648a99359
Roderick Armstrong Jr <strongz@Rodericks-MacBook-Pro.local>
2019-05-20 13:05:16 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,7 +1,26 @@
-console.log("Up and running!");
+/* console.log("up and running!"); */
+
+/*
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
+*/
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo); 
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
